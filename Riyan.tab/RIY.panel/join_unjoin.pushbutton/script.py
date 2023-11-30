89660d6eadfb18fd985a1a50538ef0860732be76
Asanka Dharmarathna <asankadharmarathna78@gmail.com>
2023-11-30 14:02:19 +0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	extension.json.txt

--- Riyan.tab/RIY.panel/join_unjoin.pushbutton/script.py
+++ Riyan.tab/RIY.panel/join_unjoin.pushbutton/script.py
@@ -1,3 +1,4 @@
+
 import clr
 clr.AddReference('RevitAPI')
 clr.AddReference('RevitAPIUI')
@@ -21,9 +22,9 @@ def join_elements(elements_to_join):
     with Transaction(doc, "Join Elements") as trans:
         trans.Start()
         for pair in elements_to_join:
-            wall, framing = pair
-            if not JoinGeometryUtils.AreElementsJoined(doc, wall, framing):
-                JoinGeometryUtils.JoinGeometry(doc, wall, framing)
+            elem1, elem2 = pair
+            if not JoinGeometryUtils.AreElementsJoined(doc, elem1, elem2):
+                JoinGeometryUtils.JoinGeometry(doc, elem1, elem2)
         trans.Commit()
 
 # Function to unjoin elements
@@ -31,30 +32,29 @@ def unjoin_elements(elements_to_unjoin):
     with Transaction(doc, "Unjoin Elements") as trans:
         trans.Start()
         for pair in elements_to_unjoin:
-            wall, framing = pair
-            if JoinGeometryUtils.AreElementsJoined(doc, wall, framing):
-                JoinGeometryUtils.UnjoinGeometry(doc, wall, framing)
+            elem1, elem2 = pair
+            if JoinGeometryUtils.AreElementsJoined(doc, elem1, elem2):
+                JoinGeometryUtils.UnjoinGeometry(doc, elem1, elem2)
         trans.Commit()
 
 # Function to collect elements from user-selected categories
 def collect_elements_from_selected_categories(selected_categories):
-    elements = []
+    elements = {}
     for category_name in selected_categories:
         bic = getattr(BuiltInCategory, 'OST_' + category_name.replace(" ", ""))
         category_filter = ElementCategoryFilter(bic)
         collector = FilteredElementCollector(doc).WherePasses(category_filter).WhereElementIsNotElementType().ToElements()
-        elements.extend(collector)
+        elements[category_name] = list(collector)
     return elements
 
-# Function to pair elements (this should be customized based on your logic)
-def get_paired_elements(elements):
+# Function to pair elements (generalized for any two categories)
+def get_paired_elements(elements, selected_categories):
     paired_elements = []
-    walls = [e for e in elements if e.Category.Name == 'Walls']
-    framings = [e for e in elements if e.Category.Name == 'Structural Framing']
-    # Simple pairing logic (you need to implement actual logic based on your requirements)
-    for wall in walls:
-        for framing in framings:
-            paired_elements.append((wall, framing))
+    if len(selected_categories) == 2:
+        cat1, cat2 = selected_categories
+        for elem1 in elements[cat1]:
+            for elem2 in elements[cat2]:
+                paired_elements.append((elem1, elem2))
     return paired_elements
 
 # Main script execution
@@ -63,9 +63,9 @@ selected_categories = forms.SelectFromList.show(all_model_categories,
                                                 multiselect=True,
                                                 button_name='Select Categories')
 
-if selected_categories:
+if selected_categories and len(selected_categories) == 2:
     elements = collect_elements_from_selected_categories(selected_categories)
-    paired_elements = get_paired_elements(elements)
+    paired_elements = get_paired_elements(elements, selected_categories)
     if paired_elements:
         join_unjoin_choice = forms.CommandSwitchWindow.show(['Join', 'Unjoin'], message='Choose an action:')
         if join_unjoin_choice == 'Join':
